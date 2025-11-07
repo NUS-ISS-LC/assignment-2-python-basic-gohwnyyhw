bf98c01bb3978929fa44d94b35f98d24c159cfa8
gohwnyyhw <gohwnyy@gmail.com>
2025-11-07 16:31:25 +0000

    Completed Assignment 2 (Goh Ching Tard)

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
 def find(s, n):
 # write your implementation here
+    pair = [0,0]
+    for first_index in range(0,len(s)-1):
+        for second_index in range(1+first_index,len(s)):
+            if s[first_index] + s[second_index] == n:
+                pair[0] = first_index
+                pair[1] = second_index
+                return pair
     return None
+print(find([2,7,11,15],9))
